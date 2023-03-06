9f1ae661ef4cd4f6172dd96150735489e444338a
thiagomorais16 <thiagomorais200@gmail.com>
2023-03-06 12:45:12 -0300

    adiocionamento SASS e atualização na página geral

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './index.scss'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
         <h1>home</h1>
         <button>
             <Link to={"/products/1"}>Produto</Link>
--- src/views/Login/login.jsx
+++ src/views/Login/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.scss'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
