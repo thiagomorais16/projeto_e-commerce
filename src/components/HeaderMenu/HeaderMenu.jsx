d61fbc3c5f3e50e6ebf1149ea51b6bbf5a7caa57
thiagomorais16 <thiagomorais200@gmail.com>
2023-03-14 17:25:20 -0300

    header menu editada

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -5,29 +5,35 @@ import "./index.scss"
 const HeaderMenu = () => {
   return  (
   <header className='headerMenu'>
+    <div id='cima'>
       <nav>
-        <div className='logo'>
-        <a href="#">  <img src="/src/assets/image_logo.png" alt="logo DNC" /> </a>
+        <div id='logo'>
+        <img id='logo1' src="/src/assets/image_logo.png" alt="logo DNC" /> 
         </div>
         <div className='search-bar'>
-          <form action="text">
-            <input type="text" img src='../../assets/image_seach.png' placeholder="O que você está procurando?" />
-          </form>
+          <label htmlFor="search">
+            <img src="/src/assets/image_seach.png" alt="ícone de pesquisa" />
+            <input type="text" placeholder='O que você está procurando?' />
+          </label>
         </div>
-        <div className='carrinho'>
+        <div id='carrinho'>
           <a href="#"> <img src="/src/assets/image_carrinho.png" alt="carrinho" className='img2' /> </a>
         </div>
       </nav>
-    <main className='menu'>
-      <ul className='animated-list'>
-        <li>Novidades</li>
-        <li>Jogos</li>
-        <li>Video Games</li>
-        <li>Mesa Gamer</li>
-        <li>Promoções</li>
-        <li>Atendimento</li>
-      </ul>
-    </main>
+    </div>
+    <div id='baixo'>
+
+      <main className='menu'>
+        <ul className='animated-list'>
+          <li>Novidades</li>
+          <li>Jogos</li>
+          <li>Video Games</li>
+          <li>Mesa Gamer</li>
+          <li>Promoções</li>
+          <li>Atendimento</li>
+        </ul>
+      </main>
+    </div>
     </header>
     
   )
