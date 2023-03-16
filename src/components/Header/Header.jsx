fde570c4bedb8473666add1150ce2ac0a0972b67
thiagomorais16 <thiagomorais200@gmail.com>
2023-03-16 16:29:10 -0300

    editado header e headerMenu e seus index

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./index.scss"
+import dncLogo from"../../assets/image_logo.png";
 import { Link } from 'react-router-dom';
 const Header = () => {
   return (
   <header className='header'>
-    <div id='menu'>
-
-    <img src="/src/assets/image_logo.png" alt="logo da DNC"/>
     
-    </div>
+
+    <img src={dncLogo} alt="logo da DNC"/>
+    
+    
    
   </header>
   )
