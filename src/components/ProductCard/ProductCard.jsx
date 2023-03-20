c6c40b1b5d47a0298397045e6511f7371cabd1d0
OlympioBelli <119749195+OlympioBelli@users.noreply.github.com>
2023-03-20 00:23:14 -0300

    Add files via upload

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const ProductCard = () => {
-  return <div>ProductCard</div>
+const ProductCard = ({data}) => {
+  console.log(data)
+  return <div className='home__card'>
+            <img src={data.imgPath} className='home_img_product'></img>
+            <h5 className='home__h5'>{data.title}</h5>
+            <button className='home_button'>
+              <Link to = {`/products/${data.id}`} className='home__router'>Ver Mais</Link>
+            </button>
+
+   
+  </div>
 };
 
 export default ProductCard;
