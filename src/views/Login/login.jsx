e3fcca1b483039e06a2cc39b88b0e2d7d12075ed
joaopedroduarte470 <118610057+joaopedroduarte470@users.noreply.github.com>
2023-04-23 22:46:23 +0100

    Login - adicionando pagina
    Adicionando ficheiros jsx e de estilos

--- src/views/Login/login.jsx
+++ src/views/Login/login.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from 'react';
 import './index.scss'
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header/Header';
 
-const Login = () => {
-  return (
-    <div>
-      <Header />
-      <h1>
-      login
-      </h1>
-      <button>
-       <Link to = {`/home`}> Home</Link>
-      </button>
 
-    </div>
-  )
-}
+  const Login = () => {
+    return (
+      <div>
+        <Header />
+        <h1>
+          Acesse com seu login ou cadastre-se!
+        </h1>
+        <h2>você pode entrar com o seu CPF</h2>
 
-export default Login
+        <div className="App">
+          <form>
+            <label>
+              Digite seu CPF:
+            </label><br></br>
+            <input type="text"></input><br></br><br></br>
+            <label>
+              Senha:
+            </label><br></br>
+            <input type="password"></input><br></br>
+            <button><Link to={`/home`}> Entrar</Link></button>
+          </form>
+        </div>
+
+      </div>
+    )
+  }
+
+  export default Login
