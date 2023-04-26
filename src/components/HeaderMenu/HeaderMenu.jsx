cafb8799b3bbb5697029be1a9ddaab2d8c31bfd1
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-04-25 22:04:37 -0300

    update headerMenu

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,51 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import dncLogo from"../../assets/image_logo.png";
-
-import "./index.scss"
+import React from "react";
+import dncLogo from "../../assets/image_logo.png";
+import carrinho from "../../assets/image_carrinho.png";
+import lupa from "../../assets/image_seach.png";
+import "./index.scss";
 
 const HeaderMenu = () => {
-  return  (
-  <header className='headerMenu'>
-    <div id='cima'>
-      <nav>
-        <div id='logo'>
-         <img id='logo1' src={dncLogo} alt="logo DNC" />  
-        
-        </div>
-        <div className='search-bar'>
-          <label htmlFor="search">
-            <img src="/src/assets/image_seach.png" alt="ícone de pesquisa" />
-            <input type="text" placeholder='O que você está procurando?' />
-          </label>
-        </div>
-        <div id='carrinho' >
-          
-            <img src="/src/assets/image_carrinho.png" alt="carrinho" className='img2' 
-            onClick={()=>
-              document.getElementById('dialogPay').showModal()
-            } />
-          
-        </div> 
-      </nav>
-    </div>
-    <div id='baixo'>
-
-      <main className='menu'>
-        <ul className='animated-list'>
-          <li>Novidades</li>
-          <li>Jogos</li>
-          <li>Video Games</li>
-          <li>Mesa Gamer</li>
-          <li>Promoções</li>
-          <li>Atendimento</li>
-        </ul>
-      </main>
-    </div>
+  return (
+    <header className="headerMenu">
+      <div id="cima">
+        <nav>
+          <div id="logo">
+            <img id="logo1" src={dncLogo} alt="logo DNC" />
+          </div>
+          <div className="search-bar">
+            <label htmlFor="search">
+              <img src={lupa} alt="ícone de pesquisa" />
+              <input type="text" placeholder="O que você está procurando?" />
+            </label>
+          </div>
+          <div id="carrinho">
+            <img
+              src={carrinho}
+              alt="carrinho"
+              className="img2"
+              onClick={() => document.getElementById("dialogPay").showModal()}
+            />
+          </div>
+        </nav>
+      </div>
+      <div id="baixo">
+        <main className="menu">
+          <ul className="animated-list">
+            <li>Novidades</li>
+            <li>Jogos</li>
+            <li>Video Games</li>
+            <li>Mesa Gamer</li>
+            <li>Promoções</li>
+            <li>Atendimento</li>
+          </ul>
+        </main>
+      </div>
     </header>
-    
-  )
-  
+  );
 };
 
 export default HeaderMenu;
